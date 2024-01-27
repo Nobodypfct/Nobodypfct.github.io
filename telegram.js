ccaca66a7c046794c501637444c1bf06b6ec4484
Nikolai Doinikov <56079901+Nobodypfct@users.noreply.github.com>
2024-01-27 09:39:20 +0300

    test commit

--- telegram.js
+++ telegram.js
@@ -2,6 +2,6 @@ const telegram = window.Telegram.WebApp
 
 window.addEventListener('load', () => {
     telegram.expand()
-    telegram.viewportStableHeight = '100%'
+    telegram.viewportStableHeight = 1000
 })
 
